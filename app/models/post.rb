b8ab9b6dc5756baa7e9b282c606aae19464157b4
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-06-30 18:56:04 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,5 +6,12 @@ class Post < ActiveRecord::Base
 
   accepts_nested_attributes_for :categories, reject_if: proc { |attributes| attributes['name'].blank? }
   
-  
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      unless category_attribute["name"].blank?
+        category = Category.find_or_create_by(category_attribute)
+        self.post_categories.build(category: category) unless self.categories.include?(category)
+      end
+    end
+  end
 end
